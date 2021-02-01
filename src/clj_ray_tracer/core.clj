bbb7f8377c7dc8aeeb3b3bad52d9037f538e62d3
Paulo Casaretto <pcasaretto@gmail.com>
2021-02-01 11:27:05 -0300

    Add aprox and aprox-vector for comparing doubles

--- src/clj_ray_tracer/core.clj
+++ src/clj_ray_tracer/core.clj
@@ -7,23 +7,33 @@
 (s/def ::tuple3d (s/tuple ::valid_double ::valid_double ::valid_double))
 
 (def epsilon 1e-10)
+
 (defn aprox [x y]
   (->
    (- x y)
    (math/abs)
    (<= epsilon)))
 
-(->
-  (- 0.99999 1)
-  (math/abs)
-  (<= epsilon))
+(s/fdef aprox
+  :args (s/cat :x ::valid_double :y ::valid_double)
+  :ret boolean?)
+
+(defn aprox-vector [v1 v2]
+  (->> (interleave v1 v2)
+      (partition 2 2)
+      (mapv (partial apply aprox))
+      (every? true?)))
+
+(s/fdef aprox-vector
+  :args (s/cat :v1 ::tuple3d :v2 ::tuple3d)
+  :ret boolean?)
 
 (defn vector-sum
   ( [v1] v1)
   ( [ v1 & more]
     (->> (apply interleave v1 more)
         (partition 2 (inc ( count more)))
-        (mapv #(apply + %)))))
+        (mapv (partial apply +)))))
 
 (s/fdef vector-sum
         :args (s/+ ::tuple3d)
@@ -34,7 +44,7 @@
   ( [ v1 & more]
    (->> (apply interleave v1 more)
         (partition 2 (inc ( count more)))
-        (mapv #(apply - %)))))
+        (mapv (partial apply -)))))
 
 (s/fdef vector-subtraction
         :args (s/+ ::tuple3d)
@@ -58,15 +68,13 @@
   :ret ::valid_double)
 
 (defn vector-normalization [v]
-  (mapv #(/ % (vector-magnitude v)) v))
+  (let [magnitude (vector-magnitude v)]
+    (mapv #(/ % magnitude) v)))
 
 (s/fdef vector-normalization
   :args (s/cat :v ::tuple3d)
   :ret ::tuple3d
   :fn #(aprox 1.0 (vector-magnitude (:ret %))))
-
-(-> [9.015341061873074E87 -1.3670317029893825E101 -2.6958168392985234E173]
-  (vector-magnitude))
 
 (defn -main
   "I don't do a whole lot ... yet."
