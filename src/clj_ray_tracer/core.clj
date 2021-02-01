ff25fd35ac95a32cd6c6614ecb09f766e84ccebe
Paulo Casaretto <pcasaretto@gmail.com>
2021-02-01 09:51:58 -0300

    Add a few more vector operations

--- src/clj_ray_tracer/core.clj
+++ src/clj_ray_tracer/core.clj
@@ -1,35 +1,72 @@
 (ns clj-ray-tracer.core
-  (:require [clojure.spec.alpha :as s])
+  (:require [clojure.spec.alpha :as s]
+            [clojure.math.numeric-tower :as math])
   (:gen-class))
 
-(s/def ::valid_double (s/double-in :NaN? false :infinite? false))
+(s/def ::valid_double (s/double-in :NaN? false :infinite? false :min -2e9 :max 2e9))
 (s/def ::tuple3d (s/tuple ::valid_double ::valid_double ::valid_double))
 
-(defn vector-sum [ v1 v2]
-  (->> (interleave v1 v2)
-       (partition 2 2)
-       (map #(apply + %))))
+(def epsilon 1e-10)
+(defn aprox [x y]
+  (->
+   (- x y)
+   (math/abs)
+   (<= epsilon)))
+
+(->
+  (- 0.99999 1)
+  (math/abs)
+  (<= epsilon))
+
+(defn vector-sum
+  ( [v1] v1)
+  ( [ v1 & more]
+    (->> (apply interleave v1 more)
+        (partition 2 (inc ( count more)))
+        (mapv #(apply + %)))))
 
 (s/fdef vector-sum
         :args (s/+ ::tuple3d)
         :ret ::tuple3d)
 
-(defn vector-subtraction [v1 v2]
-  (->> (interleave v1 v2)
-       (partition 2 2)
-       (map #(apply - %))))
+(defn vector-subtraction
+  ( [v1] v1)
+  ( [ v1 & more]
+   (->> (apply interleave v1 more)
+        (partition 2 (inc ( count more)))
+        (mapv #(apply - %)))))
 
 (s/fdef vector-subtraction
         :args (s/+ ::tuple3d)
         :ret ::tuple3d)
 
-(defn vector-negation [v1]
-   (map - v1))
+(defn vector-negation [v]
+   (mapv - v))
 
 (s/fdef vector-negation
-        :args ::tuple3d
+        :args (s/cat :v ::tuple3d)
         :ret ::tuple3d)
 
+(defn vector-magnitude [v]
+  (->> v
+    (map #(math/expt % 2))
+    (reduce +)
+    (math/sqrt)))
+
+(s/fdef vector-magnitude
+  :args (s/cat :v ::tuple3d)
+  :ret ::valid_double)
+
+(defn vector-normalization [v]
+  (mapv #(/ % (vector-magnitude v)) v))
+
+(s/fdef vector-normalization
+  :args (s/cat :v ::tuple3d)
+  :ret ::tuple3d
+  :fn #(aprox 1.0 (vector-magnitude (:ret %))))
+
+(-> [9.015341061873074E87 -1.3670317029893825E101 -2.6958168392985234E173]
+  (vector-magnitude))
 
 (defn -main
   "I don't do a whole lot ... yet."
--- test/clj_ray_tracer/core_test.clj
+++ test/clj_ray_tracer/core_test.clj
@@ -7,29 +7,35 @@
 
 (-> (stest/enumerate-namespace `clj-ray-tracer.core) stest/check)
 
-(deftest vector-create
+(deftest vector-create-test
   (testing "creating a vector"
-    (is (= (vector 3 45 5) [3 45 5]))))
+    (is (=
+         [3 45 5]
+         (vector 3 45 5)))))
 
-(deftest vector-arithmetic-sum
+(deftest vector-sum-test
+  (testing "one argument"
+    (is (=
+         [3 4 5]
+         (core/vector-sum [3 4 5]))))
   (testing "simple vector sum"
     (is (=
-         (core/vector-sum [3 4 5] [2 5 6])
-         [5 9 11])))
+         [5 9 11]
+         (core/vector-sum [3 4 5] [2 5 6]))))
   (testing "identity property"
     (let
       [v1 (gen/generate (s/gen ::core/tuple3d))]
       (is (=
-           (core/vector-sum v1 [0 0 0])
-           v1))))
+           v1
+           (core/vector-sum v1 [0 0 0])))))
   (testing "commutative property"
     (let
       [
        v1 (gen/generate (s/gen ::core/tuple3d))
        v2 (gen/generate (s/gen ::core/tuple3d))]
       (is (=
-           (core/vector-sum v1 v2)
-           (core/vector-sum v2 v1)))))
+           (core/vector-sum v2 v1)
+           (core/vector-sum v1 v2)))))
   (testing "associative propery"
     (let
       [
@@ -37,37 +43,59 @@
        v2 (gen/generate (s/gen ::core/tuple3d))
        v3 (gen/generate (s/gen ::core/tuple3d))]
       (is (=
-           (core/vector-sum (core/vector-sum v1 v2) v3)
-           (core/vector-sum v1 (core/vector-sum v2 v3)))))))
+           (core/vector-sum v1 (core/vector-sum v2 v3))
+           (core/vector-sum (core/vector-sum v1 v2) v3))))))
 
-(deftest vector-arithmetic-subtration
+(deftest vector-subtration-test
   (testing "simple vector subtraction"
     (is (=
-         (core/vector-subtraction [3 4 5] [2 5 6])
-         [1 -1 -1])))
+         [1 -1 -1]
+         (core/vector-subtraction [3 4 5] [2 5 6]))))
   (testing "identity property"
     (let
       [v1 (gen/generate (s/gen ::core/tuple3d))]
       (is (=
-           (core/vector-subtraction v1 [0 0 0])
-           v1))))
+           v1
+           (core/vector-subtraction v1 [0 0 0])))))
   (testing ""
     (let
       [
        v1 (gen/generate (s/gen ::core/tuple3d))]
       (is (=
-           (core/vector-subtraction v1 v1)
-           [0.0 0.0 0.0])))))
+           [0.0 0.0 0.0]
+           (core/vector-subtraction v1 v1))))))
 
-(deftest vector-arithmetic-negation
+(deftest vector-negation-test
   (testing "simple vector negation"
     (is (=
-         (core/vector-negation [3 4 5])
-         [-3 -4 -5])))
+         [-3 -4 -5]
+         (core/vector-negation [3 4 5]))))
   (testing "a vector and its counterpart cancel each other"
     (let
       [
        v1 (gen/generate (s/gen ::core/tuple3d))]
       (is (=
-           (core/vector-sum v1 (core/vector-negation v1))
-           [0.0 0.0 0.0])))))
+           [0.0 0.0 0.0]
+           (core/vector-sum v1 (core/vector-negation v1)))))))
+
+(deftest vector-magnitude-test
+  (testing "magnitude of a one dimensional unit vector is one"
+    (is (=
+         1
+         (core/vector-magnitude [1 0 0])))
+    (is (=
+         1
+         (core/vector-magnitude [0 1 0])))
+    (is (=
+         1
+         (core/vector-magnitude [0 0 1]))))
+  (testing "perfect squares"
+    (is (=
+          13
+          (core/vector-magnitude [3 4 12])))))
+
+(deftest vector-normalization-test
+  (testing "simple normalization"
+    (is (=
+         [1 0 0]
+         (core/vector-normalization [4 0 0])))))
