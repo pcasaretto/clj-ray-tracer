7f053af376b76fdd1616882b163e92db6860d925
Paulo Casaretto <pcasaretto@gmail.com>
2021-02-11 23:24:38 -0300

    Add initial canvas output

--- src/clj_ray_tracer/core.clj
+++ src/clj_ray_tracer/core.clj
@@ -1,7 +1,7 @@
 (ns clj-ray-tracer.core
-            [clj-ray-tracer.canvas :as canvas]
+  (:require [clj-ray-tracer.canvas :as canvas]
             [clj-ray-tracer.color :as color]
-            [clj-ray-tracer.ppm :as ppm]
+            [clj-ray-tracer.ppm :as ppm])
   (:gen-class))
 
 
@@ -12,6 +12,6 @@
     [canvas (-> (canvas/canvas :width 5 :height 3)
                 (canvas/assoc-pixel [0 0] ( color/color :red 1.5))
                 (canvas/assoc-pixel [2 1] ( color/color :green 0.5))
-                (canvas/assoc-pixel [4 2] ( color/color :red -0.5 :blue 1.0)))]
-    bitmap (ppm/canvas->ppm canvas))
-  (println bitmap))
+                (canvas/assoc-pixel [4 2] ( color/color :red -0.5 :blue 1.0)))
+     bitmap (ppm/canvas->ppm canvas)]
+    (println bitmap)))
