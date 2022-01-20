06a23b65709376229c6c8347df831d09dd79d80e
Paulo Casaretto <pcasaretto@gmail.com>
2022-01-20 18:51:49 -0300

    clocking out!

--- src/clj_ray_tracer/core.clj
+++ src/clj_ray_tracer/core.clj
@@ -1,16 +1,24 @@
 (ns clj-ray-tracer.core
   (:require [clj-ray-tracer.canvas :as canvas]
             [clj-ray-tracer.color :as color]
-            [clj-ray-tracer.ppm :as ppm])
+            [clj-ray-tracer.transform :as transform]
+            [clj-ray-tracer.ppm :as ppm]
+            [clj-ray-tracer.vector :as vector])
   (:gen-class))
 
 
 (defn -main
   [& args]
   (let
-    [canvas (-> (canvas/canvas :width 5 :height 3)
-                (canvas/assoc-pixel [0 0] (color/color :red 1.5))
-                (canvas/assoc-pixel [2 1] (color/color :green 0.5))
-                (canvas/assoc-pixel [4 2] (color/color :red -0.5 :blue 1.0)))
+    [canvas (canvas/canvas :width 150 :height 150)
+     color (color/color :red 255.0 :green 0.0 :blue 0.0)
+     canvas (->> {:x 0 :y 0 :z 0}
+             (repeat 12)
+             (map #(transform/apply % (transform/translation {:x 50 :y 0 :z 0})))
+             (map-indexed #(transform/apply %2 (transform/z-rotation (* %1 (/ Math/PI 6)))))
+             (map #(vector/+ {:x 75 :y 75 :z 0} %))
+             (map #(vector/update-map (fn [v] (Math/floor v)) %))
+             (map #(vector/update-map int %))
+             (reduce (fn [c pixel] ( canvas/assoc-pixel c [(:x pixel) (:y pixel)] color) ) canvas))
      bitmap (ppm/canvas->ppm canvas)]
     (println bitmap)))
